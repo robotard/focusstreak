50b53f1c2f5cc661a17315b4892c36df52743b1a
Jesper Thomschütz <jesper@jespersaur.com>
2013-01-22 21:58:27 +0000

    Remove accidently checked in ruby gems

--- vendor/ruby/1.9.1/gems/rufus-tokyo-1.0.7/lib/rufus/edo/ntyrant.rb
+++
@@ -1,4 +0,0 @@
-
-require 'rufus/edo/ntyrant/abstract'
-require 'rufus/edo/ntyrant/table'
-
--- vendor/ruby/1.9.1/gems/rufus-tokyo-1.0.7/spec/stop_tyrants.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# stopping the spec ttservers
-
-ruby -e "%w{ tmp/t_spec.pid tmp/tt_spec.pid }.each { |pf| File.exist?(pf) && Process.kill(9, File.read(pf).strip.to_i) }"
-
-rm tmp/t_spec.pid
-rm tmp/tt_spec.pid
-
