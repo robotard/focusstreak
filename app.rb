2893708b423a34dcebe949a8c0c438444d73bd47
Jesper Thomschütz <jesper@jespersaur.com>
2013-01-26 22:47:27 +0000

    Enable account deletion from the settings page

--- app.rb
+++ app.rb
@@ -1,5 +1,17 @@
 require 'rack/csrf'
 
+class DeleteReason
+  include MongoMapper::Document
+
+  key :email, String, :required => true
+  key :reason, String, :required => true
+  key :account_age_in_days, Float, :required => true
+  key :account_created, Time, :required => true
+
+  timestamps!
+end
+
+
 class MmUser
   include MongoMapper::Document
 
@@ -33,11 +45,6 @@ class Focusstreak < Sinatra::Base
     haml :signup
   end
 
-  get '/user/:nickname' do
-    @user = MmUser.find_by_nickname(params[:nickname])
-    haml :show
-  end
-
   post '/signup' do
     user_params = params[:user]
 
@@ -53,9 +60,36 @@ class Focusstreak < Sinatra::Base
       session[:user] = @user.id
       redirect '/'
     else
+      @email = user_params[:email]
       @error =  "#{@user.errors}."
       haml :signup
     end
+  end
+
+  get '/delete_account' do
+    login_required
+    @page_title = "Delete Account"
+    haml :delete_account
+  end
+
+  post '/delete_account' do
+    login_required
+    @page_title = "Delete Account"
+
+    if not User.authenticate(current_user.email, params[:password])
+      @reason = params[:reason]
+      @error = "Incorrect Password"
+      return haml :delete_account
+    end
+
+    DeleteReason.create(:email => current_user.email,
+                        :reason => params[:reason],
+                        :account_created => current_user.created_at,
+                        :account_age_in_days => ((Time.now - current_user.created_at) / 60 / 60 / 24).round(3))
+
+    current_user.destroy
+    session[:user] = nil
+    redirect '/'
   end
 
   get '/settings' do
@@ -82,7 +116,7 @@ class Focusstreak < Sinatra::Base
 
     user = current_user
 
-    if Digest::SHA1.hexdigest(password+salt) != user.hashed_password
+    if not User.authenticate(user.email, password)
       @error = "'Current password' was incorrect"
     elsif user.update(user_attributes)
       @notice = "Settings updated."
@@ -95,4 +129,10 @@ class Focusstreak < Sinatra::Base
 
   # Kept at the bottom so we can overwrite default routes
   register Sinatra::SinatraAuthentication
+
+  helpers do
+    def h(text)
+      Rack::Utils.escape_html(text)
+    end
+  end
 end
